162de29daaa071c5a1f590c21232de854cd15517
Mizanur Rahman <mizanurrahman615@gmail.com>
2021-06-26 15:28:12 +0600

    new template added

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -1,5 +1,27 @@
 <template>
-  <h2>Ok</h2>
-  <inertia-link href="/about">About</inertia-link> <br />
-  <inertia-link href="/contact">Contact</inertia-link>
+  <app-layout>
+    <header class="bg-white shadow">
+      <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        <h1 class="text-3xl font-bold text-gray-900">Dashboard</h1>
+      </div>
+    </header>
+    <main>
+      <div class="max-w-7xl mx-auto py-6 sm:-6 lg:px-8">
+        <div class="px-4 py-6 sm:px-0">
+          <div
+            class="border-4 border-dashed border-gray-200 round-lg h-96"
+          ></div>
+        </div>
+      </div>
+    </main>
+  </app-layout>
 </template>
+
+<script>
+import AppLayout from "../Layouts/App";
+export default {
+  components: {
+    AppLayout,
+  },
+};
+</script>
